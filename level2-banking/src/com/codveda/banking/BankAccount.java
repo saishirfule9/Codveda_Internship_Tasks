fb6c0dfbef7b5c5ef852d5259943cb3905fb05fb
Sai Shirfule <saishirfule9@gmail.com>
2025-12-02 16:06:51 +0530

    Add Bank Account class

---
+++ level2-banking/src/com/codveda/banking/BankAccount.java
@@ -0,0 +1,35 @@
+package com.codveda.banking;
+
+public class BankAccount {
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountHolder, double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance && amount > 0) {
+            balance -= amount;
+        } else {
+            System.out.println("Invalid or insufficient balance");
+        }
+    }
+}
