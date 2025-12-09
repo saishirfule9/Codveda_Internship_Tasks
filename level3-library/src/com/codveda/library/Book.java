97d77d864768bf605fa9dcd761beaa9a233cedf7
Sai Shirfule <saishirfule9@gmail.com>
2025-12-09 11:57:20 +0530

    Create Book.java

---
+++ level3-library/src/com/codveda/library/Book.java
@@ -0,0 +1,26 @@
+package com.codveda.library;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public boolean isAvailable() { return isAvailable; }
+    public void setAvailable(boolean available) { isAvailable = available; }
+
+    @Override
+    public String toString() {
+        return "Book ID: " + id + ", Title: " + title + ", Author: " + author + ", Available: " + isAvailable;
+    }
+}
